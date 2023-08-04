3e7e046bb0123f3633f19de43a8268f7cda291c5
Fausto Bernard Melo Soares <fausto.ifs@gmail.com>
2023-08-04 17:22:41 -0300

    Padrao - Chain of Responsibility

--- src/br/com/alura/loja/orcamento/Orcamento.java
+++ src/br/com/alura/loja/orcamento/Orcamento.java
@@ -5,9 +5,19 @@ import java.math.BigDecimal;
 public class Orcamento {
 
     private BigDecimal valor;
+    private int quantidadeItens;
 
     public Orcamento(BigDecimal valor) {
         this.valor = valor;
+    }
+
+    public Orcamento(BigDecimal valor, int quantidadeItens) {
+        this.valor = valor;
+        this.quantidadeItens = quantidadeItens;
+    }
+
+    public int getQuantidadeItens() {
+        return quantidadeItens;
     }
 
     public BigDecimal getValor() {
