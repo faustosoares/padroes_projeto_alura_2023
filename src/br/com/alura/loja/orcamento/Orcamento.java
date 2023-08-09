d1c1ed26a7998c13fcdd24d027f50ddc62d00f5c
Fausto Bernard Melo Soares <fausto.ifs@gmail.com>
2023-08-09 12:05:15 -0300

    Padrao - State

--- src/br/com/alura/loja/orcamento/Orcamento.java
+++ src/br/com/alura/loja/orcamento/Orcamento.java
@@ -2,10 +2,15 @@ package br.com.alura.loja.orcamento;
 
 import java.math.BigDecimal;
 
+import br.com.alura.loja.orcamento.situacao.EmAnalise;
+import br.com.alura.loja.orcamento.situacao.SituacaoOrcamento;
+
 public class Orcamento {
 
     private BigDecimal valor;
     private int quantidadeItens;
+
+    private SituacaoOrcamento situacao;
 
     public Orcamento(BigDecimal valor) {
         this.valor = valor;
@@ -14,6 +19,24 @@ public class Orcamento {
     public Orcamento(BigDecimal valor, int quantidadeItens) {
         this.valor = valor;
         this.quantidadeItens = quantidadeItens;
+        this.situacao = new EmAnalise();
+    }
+    
+    public void aplicarDescontoExtra() {
+    	BigDecimal valorDoDescontoExtra = this.situacao.calcularValorDescontroExtra(this);
+    	this.valor = this.valor.subtract(valorDoDescontoExtra);
+    }
+
+    public void aprovar() {
+    	this.situacao.aprovar(this);
+    }
+    
+    public void reprovar() {
+    	this.situacao.reprovar(this);
+    }
+    
+    public void finalizar() {
+    	this.situacao.finalizar(this);
     }
 
     public int getQuantidadeItens() {
@@ -23,4 +46,14 @@ public class Orcamento {
     public BigDecimal getValor() {
         return valor;
     }
+
+	public SituacaoOrcamento getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(SituacaoOrcamento situacao) {
+		this.situacao = situacao;
+	}
+    
+    
 }
